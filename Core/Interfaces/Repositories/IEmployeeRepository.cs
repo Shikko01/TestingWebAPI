533a719d814f0ff424f18e12a2a753bc4eac1945
Shikko01 <itprogramming070222@gmail.com>
2024-05-24 13:58:13 +0300

    Refactored GenericRepository and EmployeeRepository

--- Core/Interfaces/Repositories/IEmployeeRepository.cs
+++ Core/Interfaces/Repositories/IEmployeeRepository.cs
@@ -5,5 +5,6 @@ namespace Core.Interfaces.Repositories
     public interface IEmployeeRepository : IGenericRepository<Employee>
     {
         Task<IEnumerable<Employee>> GetEmployeesAboveAge(int targetAge);
+        Task<IEnumerable<Employee>> GetEmployeesByCountryAsync(string country);
     }
 }
