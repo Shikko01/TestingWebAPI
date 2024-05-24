3432b4bff37de3e24f7113ef46d4fffcbea5f4d7
Shikko01 <itprogramming070222@gmail.com>
2024-05-24 14:24:33 +0300

    Cleaned up the code and Investigation about how model state validation works with validation attributes is done.

--- Core/DTO/EmployeeCreateUpdateDTO.cs
+++ Core/DTO/EmployeeCreateUpdateDTO.cs
@@ -1,10 +1,20 @@
-﻿namespace Core.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
 {
     public class EmployeeCreateUpdateDTO
     {
+        [Required(ErrorMessage = "Employee Name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Employee Name must be between 2 and 50 characters")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Employee Last Name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Employee Last Name must be between 2 and 50 characters")]
         public string LastName { get; set; }
+
         public DateTime? BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
         public string Country { get; set; }
     }
 }
--- Core/DTO/EmployeeDTO.cs
+++ Core/DTO/EmployeeDTO.cs
@@ -6,12 +6,8 @@ namespace Core.DTO
     {
         public int EmployeeId { get; set; }
 
-        [Required(ErrorMessage = "Employee Name is required")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Employee Name must be between 2 and 50 characters")]
         public string? FirstName { get; set; }
 
-        [Required(ErrorMessage = "Employee Lasty Name is required")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Employee Last Name must be between 2 and 50 characters")]
         public string? LastName { get; set; }
 
         public DateTime BirthDate { get; set; }
